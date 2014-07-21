c2b31e4a884b8b916578fa9397b2d4e65333cec3
Ingy döt Net <ingy@cpan.org>
2014-07-21 05:39:04 +0000

    Import of INGY/Zilla-Dist-0.0.87 from CPAN.
    gitpan-cpan-distribution: Zilla-Dist
    gitpan-cpan-version:      0.0.87
    gitpan-cpan-path:         INGY/Zilla-Dist-0.0.87.tar.gz
    gitpan-cpan-author:       INGY
    gitpan-cpan-maturity:     released

--- lib/Zilla/Dist.pm
+++ lib/Zilla/Dist.pm
@@ -1,6 +1,6 @@
 use strict;
 package Zilla::Dist;
-our $VERSION = '0.0.86';
+our $VERSION = '0.0.87';
 
 use YAML::XS;
 use File::Share;
@@ -68,7 +68,7 @@ sub do_meta {
         }
         elsif (ref($value) eq 'HASH') {
             for my $kk (sort keys %$value) {
-                print "$kk\t$value->{$kk}\n";
+                print "$kk\n" unless $kk =~ /^(?:perl)$/;
             }
         }
         else {
